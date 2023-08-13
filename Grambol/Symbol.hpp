3c8e824d816e575f81d31016a3ae5469e13f151b
Hapaxia <Hapaxia@users.noreply.github.com>
2023-08-13 20:46:44 +0100

    update year to 2023

--- Grambol/Symbol.hpp
+++ Grambol/Symbol.hpp
@@ -5,7 +5,7 @@
 //
 // Symbol
 //
-// Copyright(c) 2020 M.J.Silk
+// Copyright(c) 2020-2023 M.J.Silk
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
